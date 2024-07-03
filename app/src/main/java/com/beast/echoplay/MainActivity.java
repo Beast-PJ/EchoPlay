b84570b00ca6ae3564a2c91b14c673c141079d00
Beast PJ <p.jadhav61657@gmail.com>
2024-07-03 23:44:41 +0530

    Day5

--- app/src/main/java/com/beast/echoplay/MainActivity.java
+++ app/src/main/java/com/beast/echoplay/MainActivity.java
@@ -1,149 +1,56 @@
 package com.beast.echoplay;
 
-import android.Manifest;
-import android.content.ContentResolver;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-import android.database.Cursor;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.widget.ImageButton;
-import android.widget.Toast;
-import androidx.annotation.NonNull;
+import android.widget.Switch;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.beast.echoplay.MusicPlayer;
 
 import java.util.ArrayList;
-import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements SongAdapter.OnItemClickListener {
+public class MainActivity extends AppCompatActivity {
 
-    private static final int STORAGE_PERMISSION_CODE = 1;
     private RecyclerView recyclerView;
-    private SongAdapter songAdapter;
-    private List<Song> songList;
-    private SharedPreferences sharedPreferences;
-    private SharedPreferences.Editor editor;
+    private SongAdapter musicAdapter;
+    private ArrayList<Song> songList;
+    private ArrayList<Song> playingQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sharedPreferences = getSharedPreferences("preferences", MODE_PRIVATE);
-        editor = sharedPreferences.edit();
-
-        boolean isNightMode = sharedPreferences.getBoolean("NightMode", false);
-        if (isNightMode) {
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-        } else {
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-        }
-
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        songList = new ArrayList<>();
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
-        } else {
-            fetchSongs();
-        }
+        songList = MusicUtils.getAllSongs(this);
+        playingQueue = new ArrayList<>();
+        musicAdapter = new SongAdapter(this, songList, song -> {
+            addToQueue(song);
+            Intent intent = new Intent(MainActivity.this, MusicPlayer.class);
+            intent.putExtra("queue", playingQueue);
+            startActivity(intent);
+        });
+        recyclerView.setAdapter(musicAdapter);
 
         ImageButton menuButton = findViewById(R.id.menu_btn);
-        menuButton.setOnClickListener(view -> openOptionsMenu());
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == R.id.action_night_mode) {
-            boolean isNightMode = sharedPreferences.getBoolean("NightMode", false);
+        menuButton.setOnClickListener(v -> {
+            Switch nightModeSwitch = findViewById(R.id.action_night_mode);
+            boolean isNightMode = nightModeSwitch.isChecked();
             if (isNightMode) {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-                editor.putBoolean("NightMode", false);
-                editor.apply();
-                Toast.makeText(this, "Night mode off", Toast.LENGTH_SHORT).show();
-            } else {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-                editor.putBoolean("NightMode", true);
-                editor.apply();
-                Toast.makeText(this, "Night mode on", Toast.LENGTH_SHORT).show();
-            }
-            recreate();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    private void fetchSongs() {
-        ContentResolver contentResolver = getContentResolver();
-        Cursor cursor = contentResolver.query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, null, null, null, null);
-
-        if (cursor != null && cursor.moveToFirst()) {
-            int titleColumn = cursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
-            int artistColumn = cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST);
-            int durationColumn = cursor.getColumnIndex(MediaStore.Audio.Media.DURATION);
-            int dataColumn = cursor.getColumnIndex(MediaStore.Audio.Media.DATA);
-
-            do {
-                String title = cursor.getString(titleColumn);
-                String artist = cursor.getString(artistColumn);
-                String duration = cursor.getString(durationColumn);
-                String data = cursor.getString(dataColumn);
-
-                Song song = new Song(title, artist, formatDuration(duration), data);
-                songList.add(song);
-            } while (cursor.moveToNext());
-
-            cursor.close();
-        }
-
-        songAdapter = new SongAdapter(songList, this);
-        recyclerView.setAdapter(songAdapter);
-    }
-
-    private String formatDuration(String duration) {
-        long millis = Long.parseLong(duration);
-        long minutes = (millis / 1000) / 60;
-        long seconds = (millis / 1000) % 60;
-        return String.format("%02d:%02d", minutes, seconds);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == STORAGE_PERMISSION_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                fetchSongs();
             } else {
-                Log.e("MainActivity", "Permission denied to read storage");
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
             }
-        }
+        });
     }
 
-    @Override
-    public void onItemClick(int position) {
-        Intent intent = new Intent(this, MusicPlayer.class);
-        intent.putExtra("SONG_LIST", (ArrayList<Song>) songList);
-        intent.putExtra("POSITION", position);
-        startActivity(intent);
+    private void addToQueue(Song song) {
+        playingQueue.add(song);
     }
 }
